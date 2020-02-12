c84d209ddab459878825ca215596d0581d29be27
skvoch <skvoch@gmail.com>
2020-02-12 23:17:33 +0300

    Fixed: location to test db

--- internal/app/store/store_test.go
+++ internal/app/store/store_test.go
@@ -1,20 +1,5 @@
 package store_test
 
-import (
-	"os"
-	"testing"
-)
-
 var (
-	databaseURL string
+	databaseURL string = "host=localhost dbname=burst_test sslmode=disable user=postgres password=docker"
 )
-
-func TestMain(m *testing.M) {
-	databaseURL := os.Getenv("DATABASE_URL")
-
-	if databaseURL == "" {
-		databaseURL = "host=localhost dbname=burst_test sslmode=disable user=postgres database_password = password=docker"
-	}
-
-	os.Exit(m.Run())
-}
