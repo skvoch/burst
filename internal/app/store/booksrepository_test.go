a718395669df0ee8a3829a4e9cdb6b5a9e5d0f1a
skvoch <skvoch@gmail.com>
2020-02-12 23:17:53 +0300

    Updated: test code

--- internal/app/store/booksrepository_test.go
+++ internal/app/store/booksrepository_test.go
@@ -3,6 +3,9 @@ package store_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	"github.com/skvoch/burst/internal/app/model"
 	"github.com/skvoch/burst/internal/app/store"
 )
 
@@ -10,13 +13,16 @@ func TestBooksRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("books")
 
-	b, err := s.Books().Create( &model.Book{
-		Name: "Golang book",
+	book := &model.Book{
+		ID:          0,
+		Name:        "Golang book",
 		Description: "Super cool book",
-		Review: "I want to recomend it for you!",
-		Rating: 5,
-		Type: 1,
-	}); err != nil {
-		t.Fatal()
+		Review:      "I want to recomend it for you!",
+		Rating:      5,
+		Type:        0,
 	}
+
+	err := s.Books().Create(book)
+
+	assert.NoError(t, err)
 }
