099408922962835a09c74e5db871045029235ac1
Dmitriy <skvoch@gmail.com>
2020-02-19 20:07:35 +0300

    WIP: added basic instances

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/skvoch/burst/internal/app/telegramserver"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/telegram.toml", "path to config file")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	flag.Parse()
+
+	config := telegramserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := telegramserver.New(config)
+	server.Start()
 }
