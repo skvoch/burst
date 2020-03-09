f050d0c9cb1229baa0bcdc4364fe0dfc3ba1ef75
Dmitriy <skvoch@gmail.com>
2020-03-09 14:06:18 +0300

    Feature: added overrride config fields by env

--- cmd/burst/main.go
+++ cmd/burst/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
 	"github.com/skvoch/burst/internal/app/apiserver"
@@ -16,11 +17,20 @@ func init() {
 	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
 }
 
+func setEnviromentVaribles(c *apiserver.Config) {
+	envDatabaseURL := os.Getenv("DATABASE_URL")
+
+	if envDatabaseURL != "" {
+		c.DataBaseURL = envDatabaseURL
+	}
+}
+
 func main() {
 	flag.Parse()
 
 	config := apiserver.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
+	setEnviromentVaribles(config)
 
 	if err != nil {
 		log.Fatal(err)
--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/BurntSushi/toml"
 	"github.com/skvoch/burst/internal/app/telegramserver"
@@ -16,11 +18,38 @@ func init() {
 	flag.StringVar(&configPath, "config-path", "telegram.toml", "path to config file")
 }
 
+// Override config fields if env variables is set
+func setEnviromentVaribles(c *telegramserver.Config) error {
+	envApplicationToken := os.Getenv("TELEGRAM_APPLICATION_TOKEN")
+
+	if envApplicationToken != "" {
+		c.ApplicationToken = envApplicationToken
+	}
+
+	envOwnerID := os.Getenv("OWNER_ID")
+
+	if envOwnerID != "" {
+		id, err := strconv.Atoi(envOwnerID)
+
+		if err != nil {
+			return err
+		}
+
+		c.OwnerID = id
+	}
+
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
 	config := telegramserver.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
+
+	if err := setEnviromentVaribles(config); err != nil {
+		log.Fatal(err)
+	}
 
 	if err != nil {
 		log.Fatal(err)
